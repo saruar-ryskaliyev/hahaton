97e78254367e3dd269b5fc15bff7e291ff4f2603
Bexultan_Tokan <Bexultan_Tokan>
2024-04-13 17:40:39 +0500

    check

--- script.py
+++ script.py
@@ -1,9 +1,16 @@
 import requests, json
+import google.generativeai as genai
 from bs4 import BeautifulSoup
 def fromLinkToData(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     text = soup.get_text()
     return text
+def request(text):
+    genai.configure(api_key='AIzaSyBs3c846AoPQ06gmSKt0DtIcswpOp8iwKg')
+    model = genai.GenerativeModel('gemini-1.5-pro-latest')
+    response = model.generate_content("Give me info about the cashbacks and bonuses for this card. I need you to return me as a json.\n" + text)
+    return response.text
 
-print(fromLinkToData('https://halykbank.kz/card/dostavka'))
+text = fromLinkToData('https://halykbank.kz/card/dostavka')
+print(request(text))
